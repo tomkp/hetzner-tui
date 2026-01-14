90f4e886fbda71b66f26b34de6f13201bec1294f
tomkp <tom@tomkp.com>
2026-01-14 10:01:40 +0000

    Add FirewallsScreen
    Closes #21

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -2,6 +2,7 @@ export {
   default as MainMenuScreen,
   type MenuAction,
 } from "./MainMenuScreen.js";
+export { default as FirewallsScreen } from "./FirewallsScreen.js";
 export { default as NetworksScreen } from "./NetworksScreen.js";
 export { default as ServersScreen } from "./ServersScreen.js";
 export { default as SetupWizard } from "./SetupWizard.js";
